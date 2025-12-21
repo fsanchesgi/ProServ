a42fc4228859d439dae09c0c939e5ef9cb87aca0
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-20 23:50:51 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,10 @@
-  window.supabaseClient = supabase.createClient(
-    "https://uqwbduinwugaqexsvkxc.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxd2JkdWlud3VnYXFleHN2a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk5MjMsImV4cCI6MjA4MTI5NTkyM30._GzXlkNAvqbevYjmi-crhvSKGQQfX3yjzTWT5PTvIxE"
-  );
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://uqwbduinwugaqexsvkxc.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxd2JkdWlud3VnYXFleHN2a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk5MjMsImV4cCI6MjA4MTI5NTkyM30._GzXlkNAvqbevYjmi-crhvSKGQQfX3yjzTWT5PTvIxE";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// EXPÕE GLOBALMENTE
+window.supabase = supabase;

ce2213a3e6d7d367cda4223b43273162fecd7cec
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-21 16:49:49 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://uqwbduinwugaqexsvkxc.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxd2JkdWlud3VnYXFleHN2a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk5MjMsImV4cCI6MjA4MTI5NTkyM30._GzXlkNAvqbevYjmi-crhvSKGQQfX3yjzTWT5PTvIxE";
 
-window.supabase = supabaseJs.createClient(
+window.supabase = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );

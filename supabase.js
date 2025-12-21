309fcd694872a21d762f321de6a9e14f656be785
fsanchesgi <133274266+fsanchesgi@users.noreply.github.com>
2025-12-20 22:50:46 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,4 @@
-<script>
   window.supabaseClient = supabase.createClient(
     "https://uqwbduinwugaqexsvkxc.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxd2JkdWlud3VnYXFleHN2a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk5MjMsImV4cCI6MjA4MTI5NTkyM30._GzXlkNAvqbevYjmi-crhvSKGQQfX3yjzTWT5PTvIxE"
   );
-</script>
